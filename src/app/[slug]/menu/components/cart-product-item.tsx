37187238236b92d18c39ca2dd71b87cee2bc7afb
kauamassei <gameplayultra11@gmail.com>
2025-02-22 16:55:26 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,46 +1,55 @@
 import Image from "next/image";
-import { CartProduct } from "../contexts/cart"
+import { CartContext, CartProduct } from "../contexts/cart";
 import { formatCurrency } from "@/helpers/format-currency";
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
-
+import { useContext } from "react";
 
 interface CartItemProps {
-    product: CartProduct;
+  product: CartProduct;
 }
 
-const CartProductItem = ({product}: CartItemProps) => {
+const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
-          {/* ESQUERDA */}
-        <div className="flex items-center gap-3">
+      {/* ESQUERDA */}
+      <div className="flex items-center gap-3">
         <div className="relative h-20 w-20 rounded-xl bg-gray-100">
-            <Image src={product.imageUrl} alt={product.name} fill />
+          <Image src={product.imageUrl} alt={product.name} fill />
         </div>
         <div className="space-y-1">
-            <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
-            <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
 
-            {/* QUANTIDADE */}
-            <div className="flex items-center gap-1 text-center">
-              <Button className="h-7 w-7 roounded-lg" variant="outline">
-                <ChevronLeftIcon />
-              </Button>
-              <p className="w-7 text-xs">{product.quantity}</p>
-              <Button className="h-7 w-7 roounded-lg" variant="destructive">
-                <ChevronRightIcon />
-              </Button>
-            </div>
+          {/* QUANTIDADE */}
+          <div className="flex items-center gap-1 text-center">
+            <Button
+              className="roounded-lg h-7 w-7"
+              variant="outline"
+              onClick={() => decreaseProductQuantity(product.id)}
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <p className="w-7 text-xs">{product.quantity}</p>
+            <Button className="roounded-lg h-7 w-7" variant="destructive">
+              <ChevronRightIcon />
+            </Button>
+          </div>
         </div>
-        </div>
+      </div>
 
-        {/* DELETE BUTTON */}
+      {/* DELETE BUTTON */}
 
-        <Button className="h-7 w-7 roounded-lg" variant="outline">
-          <TrashIcon />
-        </Button>
+      <Button className="roounded-lg h-7 w-7" variant="outline">
+        <TrashIcon />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default CartProductItem
+export default CartProductItem;
