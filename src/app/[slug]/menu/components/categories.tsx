14d542eb6260b3a9895b5d508c8ceb0be7492dc0
kauamassei <gameplayultra11@gmail.com>
2025-02-23 17:40:22 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import { Restaurant, Prisma} from "@prisma/client";
+import { Restaurant, Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Products from "./products";
+import { CartContext } from "../contexts/cart";
+import { formatCurrency } from "@/helpers/format-currency";
+import CartSheet from "./cart-sheet";
 
 interface RestaurantCategoriesProps {
   restaurant: Prisma.RestaurantGetPayload<{
@@ -15,21 +18,20 @@ interface RestaurantCategoriesProps {
 }
 
 type MenuCategoriesWithProducts = Prisma.MenuCategoryGetPayload<{
-    include: { products: true };
-}>
+  include: { products: true };
+}>;
 
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
-  const [selectedCategory, setSelectedCategory] = useState<MenuCategoriesWithProducts>(
-    restaurant.menuCategories[0],
-  );
-
+  const [selectedCategory, setSelectedCategory] =
+    useState<MenuCategoriesWithProducts>(restaurant.menuCategories[0]);
+  const { products, total, toggleCart, totalQuantity } = useContext(CartContext);
   const handleCategoryClick = (category: MenuCategoriesWithProducts) => {
     setSelectedCategory(category);
-  }
+  };
 
   const getCategoryButtonVariant = (category: MenuCategoriesWithProducts) => {
-    return selectedCategory.id === category.id ? "default" : "secondary"
-  }
+    return selectedCategory.id === category.id ? "default" : "secondary";
+  };
 
   return (
     <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl bg-white">
@@ -65,15 +67,26 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
               {category.name}
             </Button>
           ))}
-          
         </div>
 
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
 
-          <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
+      <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
 
-      <Products products={selectedCategory.products}/>
+      <Products products={selectedCategory.products} />
+      {products.length > 0 && (
+        <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
+          <div>
+            <p className="text-xs text-muted-foreground">Total dos pedidos</p>
+            <p className="text-sm font-semibold">
+              {formatCurrency(total)} <span className="text-xs font-normal text-muted-foreground">/{totalQuantity} {totalQuantity > 1 ? "itens" : "item"}</span>
+            </p>
+          </div>
+          <Button onClick={toggleCart}>Ver sacola</Button>
+          <CartSheet />
+        </div>
+      )}
     </div>
   );
 };
